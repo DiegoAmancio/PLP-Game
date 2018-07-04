42233980c3cfecea2156a2051b64ad5c2eaab825
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2018-07-04 11:51:04 -0300

    arrumando funcionamento jogo player vs player

--- versao haskell/jogo.hs
+++ versao haskell/jogo.hs
@@ -1,12 +1,13 @@
 import System.IO
 import System.Random
+import Control.Concurrent
 
 menuInicial :: IO ()
 menuInicial = do 
     putStrLn "1 - Versus Player        \n2 - Versus Computador         \n3 - Ajuda/Creditos\n4 - Regras        \n5 - Sair          \n"
     
     input <- getLine
-    if input == "1" then inicioPlayer False--jogo2p
+    if input == "1" then inicioPlayer False False--jogo2p
     else if input == "2" then main --jogoVsBot
     else if input == "3" then ajuda
     else if input == "4" then regras
@@ -31,19 +32,27 @@ vez versusBot timeB | versusBot  &&  not timeB  = 	"Sua vez  => digite qualquer 
                     | not versusBot && not timeB  = "Vez do Player 1  => digite qualquer coisa para rodar o dado ou desistir para sair:\n"
                     | versusBot && timeB = "Vez do bot : o bot vai jogar o dado...\n"
                     | otherwise =  "Vez do Player 2  => digite qualquer coisa para rodar o dado ou desistir para sair:\n"
+jogo :: Bool -> Bool -> IO()
+jogo versusBot timeB =
+    inicioPlayer versusBot timeB
 
-inicioPlayer :: Bool -> IO ()
+inicioPlayer :: Bool -> Bool -> IO ()
 inicioPlayer versusBot timeB = do  
-                        putStrLn (vez False timeB)
+                        putStrLn (vez versusBot timeB)
                         desistiu <- getLine
                         
-                        if desistiu == "desistir" then parouVolta False timeB else continuando False timeB
+                        if desistiu == "desistir" then parouOuVolta versusBot timeB else continuando versusBot timeB
+
 inicioBot :: Int -> IO()
-inicioBot dado = 
+inicioBot dado = do 
     putStrLn (vez True True)
-    let ss = 2
     --jogapeca
-    inicioPlayer False
+    threadDelay 2000000
+    inicioPlayer True False
+
+
+
+
 random :: Int -> IO Int
 random n = randomRIO (1, n :: Int)                    
 
@@ -51,13 +60,13 @@ continuando ::  Bool -> Bool -> IO()
 continuando versusBot timeB = 
    --dado
     --jogapeca
-    if not versusBot || timeB then inicioPlayer False
+    if  (not versusBot)  then inicioPlayer versusBot (not timeB)
     else inicioBot 2 --dado
     
 
 
-parouVolta :: Bool -> Bool  -> IO()
-parouVolta versusBot timeB =  do
+parouOuVolta :: Bool -> Bool  -> IO()
+parouOuVolta versusBot timeB =  do
     putStrLn (desistir versusBot timeB)
     reiniciar <- getLine
     if reiniciar == "sim" then menuInicial else putStrLn "jogo encerrado"                      
