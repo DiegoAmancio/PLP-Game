a4489535f250832cfe3ea41e2c891c7fccc82210
Kleberson <kleberson.canuto@ccc.ufcg.edu.br>
2018-07-18 11:14:14 -0300

    Adicionando as estruturas de dados (matriz ainda incompleta)

--- versao prolog/jogo.pl
+++ versao prolog/jogo.pl
@@ -1,8 +1,37 @@
 :- initialization main.
 
+make_peca(X,Y,Casas,Num,peca(X,Y,Casas,Num)).
+make_jogador(Peca1, Peca2, Time, jogador(Peca1,Peca2,Time)).
+make_tabuleiro(JogadorA, JogadorB, Matriz, tabuleiro(JogadorA,JogadorB,Matriz)).
+
+get_x(peca(X,_,_,_),X).
+get_y(peca(_,Y,_,_),Y).
+get_casas(peca(_,_,Casas,_),Casas).
+get_num(peca(_,_,_,Num),Num).
+
+get_peca1(jogador(Peca1,_,_),Peca1).
+get_peca2(jogador(_,Peca2,_),Peca2).
+get_time(jogador(_,_,Time),Time).
+
+get_jogadorA(tabuleiro(JogadorA,_,_),JogadorA).
+get_jogadorB(tabuleiro(_,JogadorB,_),JogadorB).
+get_matriz(tabuleiro(_,_,matriz),matriz).
+
+gera_matriz(Jog1, Jog2, X) :-
+	X = [[0],[0],[0],[0],[0]].
+
 jogo(X):-
     repeat,
-    (X == "BOT" -> write("Iniciando BOT"),nl; write("Iniciando Player"),nl),
+    (X == 0 -> write("Iniciando VS"),nl; write("Iniciando BOT"),nl),
+	make_peca(-1,-1,0,1,Peca1A),
+	make_peca(-1,-1,0,2,Peca2A),
+	make_peca(-1,-1,0,1,Peca1B),
+	make_peca(-1,-1,0,2,Peca2B),
+	make_jogador(Peca1A,Peca2A,"A",JogadorA),
+	make_jogador(Peca1B,Peca2B,"B",JogadorB),
+	gera_matriz(JogadorA, JogadorB, Tab),
+	make_tabuleiro(JogadorA,JogadorB,Tab,Tabuleiro),
+	write(Tabuleiro),nl,
     halt(0).
 
 readHelp():-
@@ -19,5 +48,5 @@ main:-
     read_line_to_codes(user_input, X1),
     string_to_atom(X1, X2),
     atom_number(X2, X),
-    (X == 1 -> jogo("Player"); X == 2 -> jogo("BOT"); X == 3 -> readHelp(); X == 4 -> readRules()),
+    (X == 1 -> jogo(0); X == 2 -> jogo(1); X == 3 -> readHelp(); X == 4 -> readRules()),
     halt(0).
