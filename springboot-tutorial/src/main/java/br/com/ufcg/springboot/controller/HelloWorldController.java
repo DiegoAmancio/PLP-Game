175038371dcd513d19420ea7675a06aa2b87494a
Diego Amancio <diego.pereira@ccc.ufcg.edu.br>
2018-07-18 14:59:10 -0300

    Add files via upload

---
+++ springboot-tutorial/src/main/java/br/com/ufcg/springboot/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package br.com.ufcg.springboot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "*")
+public class HelloWorldController {
+	
+	@GetMapping(value = "/hello")
+	public String helloWorld() {
+		return "Olá desgraça!";
+	}
+}
